64aae363406722bd168d218e75128ab8fed0088e
iwacheto <ivailo@webrika.bg>
2023-10-29 18:09:33 +0200

    Add Changes

--- app/components/views/movies/index.tsx
+++ app/components/views/movies/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Movies = ({ data }) => {
+const Movies = ({ data }: any) => {
   console.log("movies", data);
   // console.log("movies", data);
 
-  return <div>Movies
-    <div>{data.map((item: any, index: number)=>(
-        <div key={index}>{item.title}</div>
-    ))}</div>
-  </div>;
+  return (
+    <div>
+      Movies
+      <div>
+        {data.map((item: any, index: number) => (
+          <div key={index}>{item.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Movies;
