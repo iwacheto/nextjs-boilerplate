742f2eba526bb42ad231cc199908e8ca4ab17902
iwacheto <ivailo@webrika.bg>
2023-11-21 12:02:22 +0200

    Add js cdn

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-// import { client } from "../../utils/client/sanity";
-// import Movies from "../components/views/movies";
+import { client } from "../../utils/client/sanity";
+import Movies from "../components/views/movies";
 
 const About = async () => {
-  // const data = await getData();
+  const data = await getData();
 
   return (
     <section>
       About
-      {/* <Movies data={data} /> */}
+      <Movies data={data} />
     </section>
   );
 };
 
 export default About;
 
-// async function getData() {
-//   const response = await client.fetch(`*[_type == 'movie']`);
+async function getData() {
+  const response = await client.fetch(`*[_type == 'movie']`);
 
-//   if (!response) {
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!response) {
+    throw new Error("Failed to fetch data");
+  }
 
-//   return response;
-// }
+  return response;
+}
